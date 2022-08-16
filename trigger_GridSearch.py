e1abd6ba81a3284c871f8242431b79667b8dc79e
LeoAraujoEE <leo.araujo.ee@gmail.com>
2022-08-15 23:09:01 -0300

    Fixed CSV for COVIDxCT

--- trigger_GridSearch.py
+++ trigger_GridSearch.py
@@ -10,15 +10,15 @@ PATH_DICT = { "datasets": os.path.join( "D:\\", "Datasets", "COVID19", "CT", "cl
               "outputs" : os.path.join( "." ) }
 
 # List of hyperparameter values
-hyperparameter_dict = { "num_epochs":                     [5],  # Total N° of training epochs
-                        "batchsize":                     [16],  # Minibatch size
+hyperparameter_dict = { "num_epochs":                    [30],  # Total N° of training epochs
+                        "batchsize":                     [32],  # Minibatch size
                         "early_stop":                    [13],  # Early Stopping patience
-                        "input_height":                 [256],  # Model's input size
-                        "input_width":                  [256],  # Model's input size
+                        "input_height":                 [224],  # Model's input size
+                        "input_width":                  [224],  # Model's input size
                         "input_channels":                 [1],  # Model's input size
                         "start_lr":                    [1e-2],  # Starting learning rate
                         "lr_adjust_frac":               [0.5],  # Fraction to adjust learning rate
-                        "lr_adjust_freq":                 [2],  # Frequency to adjust learning rate
+                        "lr_adjust_freq":                 [5],  # Frequency to adjust learning rate
                         "optimizer":                 ["adam"],  # Chosen optimizer
                         "monitor":                 ["val_f1"],  # Monitored variable for callbacks
                         "augmentation":                [True],  # If data augmentation should be used
@@ -26,32 +26,40 @@ hyperparameter_dict = { "num_epochs":                     [5],  # Total N° of t
                         "apply_undersampling":         [True],  # Wether to apply Random Undersampling
                         "l1_reg":                         [0],  # Amount of L1 regularization
                         "l2_reg":                         [0],  # Amount of L2 regularization
-                        "base_dropout":                 [.15],  # SpatialDropout2d between blocks in convolutional base
-                        "top_dropout":                  [0.3],  # Dropout between dense layers in model top
+                        "base_dropout":  [0.0, 0.1, 0.2, 0.3],  # SpatialDropout2d between blocks in convolutional base
+                        "top_dropout":           [0.30, 0.50],  # Dropout between dense layers in model top
                         "architecture":          ["resnet50"],  # Chosen architecture
                         "seed":                          [69],  # Seed for pseudorandom generators
                       } 
 
-augmentation_dict = { "zoom_in":                 0.00,          # Max zoom in
-                      "zoom_out":                0.10,          # Max zoom out
-                      "shear":                   00.0,          # Max random shear
-                      "rotation":                 5.0,          # Max random rotation
-                      "vertical_translation":    0.05,          # Max vertical translation
-                      "horizontal_translation":  0.05,          # Max horizontal translation
-                      "vertical_flip":          False,          # Allow vertical flips  
-                      "horizontal_flip":        False,          # Allow horizontal flips    
-                      "brightness":              0.00,          # Brightness adjustment range
-                      "channel_shift":           00.0,          # Random adjustment to random channel
-                      "constant_val":            00.0,
-                      "fill_mode":          "constant"
+augmentation_dict = { "zoom_in":                         0.00,  # Max zoom in
+                      "zoom_out":                        0.10,  # Max zoom out
+                      "shear":                           00.0,  # Max random shear
+                      "rotation":                         5.0,  # Max random rotation
+                      "vertical_translation":            0.05,  # Max vertical translation
+                      "horizontal_translation":          0.05,  # Max horizontal translation
+                      "vertical_flip":                  False,  # Allow vertical flips  
+                      "horizontal_flip":                False,  # Allow horizontal flips    
+                      "brightness":                      0.00,  # Brightness adjustment range
+                      "channel_shift":                   00.0,  # Random adjustment to random channel
+                      "constant_val":                    00.0,
+                      "fill_mode":                  "constant"
                       }
 
-keep_pneumonia = True
-dataset_list = [ "miniCOVIDxCT", "Comp_CNCB_iCTCF", "miniCNCB", "radiopaedia.org", "COVID-CTSet", "COVID-CT-MD", "Comp_LIDC-SB" ]
+dataset_list = [ "COVIDxCT",        # Whole COVIDxCT-3A dataset
+                 "miniCOVIDxCT",    # Reduced COVIDxCT-3A, has only samples from used datasets
+                 "Comp_CNCB_iCTCF", # 88k / 69k - Combination of CNCB non COVID samples + iCTCF
+                 "miniCNCB",        # 74k / 55k - Rest of CNCB dataset
+                 "COVID-CT-MD",     # 23k / 20k - 
+                 "Comp_LIDC-SB",    # 18k / 18k - Combination of LIDC + Stone Brook
+                 "COVID-CTSet",     # 12k / 12k - 
+                 "radiopaedia.org", #  4k /  3k
+               ]
 
-trainManager = ModelManager( path_dict = PATH_DICT, dataset_name = "radiopaedia.org", 
+trainManager = ModelManager( path_dict = PATH_DICT, 
+                             dataset_name = "COVID-CT-MD", 
                              hyperparam_values = hyperparameter_dict, 
                              aug_params = augmentation_dict, 
-                             keep_pneumonia = keep_pneumonia )
+                             keep_pneumonia = False )
 
 trainManager.doGridSearch( shuffle = False )
